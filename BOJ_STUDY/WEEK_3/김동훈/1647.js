a0cb28006c2d35bea73cd0c3541978ace49c4f57
김동훈A <agmon5959@douzone.com>
2022-08-23 01:24:54 +0900

    포기

--- BOJ_STUDY/WEEK_3/김동훈/1647.js
+++ BOJ_STUDY/WEEK_3/김동훈/1647.js
@@ -1,2 +1,16 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-let input = Number(require("fs").readFileSync(filePath).toString().trim().split("\n"));
+let input = (require("fs").readFileSync(filePath).toString().trim().split("\n"));
+const [house, load] = input[0].split(" ");
+const path = [];
+// const graph = Array.from({ length: load + 1 }).fill(0);
+let graph = Array.from(Array(Number(house) + 1), () => Array());
+
+for (let i = 1; i < input.length; i++){
+    path.push(input[i].split(" ").map(e => +e));
+}
+
+path.forEach(([start, end, cost]) => {
+    graph[start].push([end, cost]);
+})
+
+// 포기
