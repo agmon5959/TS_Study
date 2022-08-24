fba9089870aac35aefa804f540c2c42d160fba7e
김동훈A <agmon5959@douzone.com>
2022-08-25 01:34:46 +0900

    1647 해결

--- BOJ_STUDY/WEEK_3/김동훈/1647.js
+++ BOJ_STUDY/WEEK_3/김동훈/1647.js
@@ -1,78 +1,92 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let input = (require("fs").readFileSync(filePath).toString().trim().split("\n"));
+const [house, load] = input[0].split(" ");
+const path = [];
+const parent = Array.from({ length: Number(house) + 1 }).map((iter, idx) => +idx);
 
-function main() {
-    let answer = 0;
-    const [N, M] = input[0].split(' ').map(Number);
-    let road = input.slice(1).map(_ => _.trim().split(' ').map(Number));
-
-    let village = [];
-    for (let i = 0; i < road.length; i++) {
-        let [A, B, C] = road[i];  // A -> B : C.
-        village.push({
-            from: A,
-            to: B,
-            cost: C
-        })
-    }
-    village.sort((a, b) => {
-        a = a.cost;
-        b = b.cost;
-        if (a > b) return 1;
-        else if (a < b) return -1;
-    })
-    //console.log(village)
-
-    // unionFind
-    let parent = new Array(N + 1).fill(0);
-    for (let i = 0; i <= N; i++) {
-        parent[i] = i;
-    }
-
-    answer = sol(village, parent);
-
-    return console.log(answer)
+for (let i = 1; i < input.length; i++){
+    path.push(input[i].split(" ").map(e => +e));
 }
+path.sort((a,b) => {
+    a = a[2];
+    b = b[2];
+    if (a > b) return 1;
+    else return -1;
+})
 
-function sol(village, parent) {
-    let sum = 0;
-    let MST = [];
+//! union find -- 
+// x의 루트노드 찾는 함수
+const find = (x) => {
+    // 배열의 값과 인덱스가 같다면 부모 노드의 인덱스를 반환한다.
+    if (parent[x] === x) {
+        return x;
+    }
+    // 경로 압축을 위해 부모노드의 값으로 바꿔준다.
+    const currentParent = find(parent[x]);
+    return parent[x] = currentParent;
+
+};
+
+// 두 노드를 합치기 위한 함수
+const union = (x, y) => {
     
-    for (let i = 0; i < village.length; i++) {
-        let [a, b, c] = [village[i].to, village[i].from, village[i].cost];
-        // cycle check.
-        if (!findParent(a, b, parent)) {
-            unionParent(a, b, parent);
-            MST.push({
-                from: a,
-                to: b,
-                cost: c
-            })
-        }
-    }
-    // 비용이 젤 높은 간선 자르기.
-    MST.pop()
-    for (let i = 0; i < MST.length; i++) {
-        sum += MST[i].cost;
-    }
+    x = find(x);
+    y = find(y);
 
-    return sum;
-}
-function getParent(x, parent) {
-    if (parent[x] === x) return x;
-    else return parent[x] = getParent(parent[x], parent); // return 이 있어야 축약됨.
-}
-function findParent(a, b, parent) {
-    a = getParent(a, parent);
-    b = getParent(b, parent);
-    if (a === b) return true;
+    // x와 y가 같다면 이미 연결되어 있는 경우
+    if (x === y) return;
+
+    // x와 y 둘 중 큰 값을 가지는 부모 인덱스를 작은 값을 가지는 변수로 넣는다.
+    if (x < y) parent[y] = x;
+    else parent[x] = y;
+};
+
+// 같은 부모를 가지는지 확인하는 함수
+const isUnion = (x, y) => {
+    x = find(x);
+    y = find(y);
+    
+    if (x === y) { return true; }
+    // 사이클이 생기지 않음
     else return false;
 }
-function unionParent(a, b, parent) {
-    a = parent[a];
-    b = parent[b];
-    if (a < b) parent[b] = a;
-    else parent[a] = b;
-}
 
-main();
+//? MST 구현부
+const MST = [];
+for (let i = 0; i < path.length; i++){
+    let [startNode, endNode, cost] = [path[i][0], path[i][1], path[i][2]];
+    // 사이클이 생기는지 안생기는지 체크한다.
+    if (!isUnion(startNode,endNode)) {
+        union(startNode, endNode);
+        MST.push({
+            from: startNode,
+            to: endNode,
+            cost: cost
+        })
+    }
+}
+MST.pop();
+let sum = 0;
+MST.forEach((iter) => { sum += iter.cost });
+console.log(sum);
+
+
+//@ 궁금한 점
+//@ 같은 부모를 가지고 있으면 사이클이 생성된다는 것을 어떻게 아는가?
+//@ 가장 큰 값을 빼주는 이유는 무엇인가?
+
+
+// input > 
+// 7 12
+// 1 2 3
+// 1 3 2
+// 3 2 1
+// 2 5 2
+// 3 4 4
+// 7 3 6
+// 5 1 5
+// 1 6 2
+// 6 4 1
+// 6 5 3
+// 4 5 3
+// 6 7 4
--- BOJ_STUDY/WEEK_3/김동훈/1717.js
+++ BOJ_STUDY/WEEK_3/김동훈/1717.js
@@ -1,2 +1,3 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let input = Number(require("fs").readFileSync(filePath).toString().trim().split("\n"));
+
