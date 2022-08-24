2ee36fbf869ea60bd777e0f4d0804cc15b63a5d5
김동훈A <agmon5959@douzone.com>
2022-08-24 23:03:15 +0900

    1647

--- BOJ_STUDY/WEEK_3/김동훈/1647.js
+++ BOJ_STUDY/WEEK_3/김동훈/1647.js
@@ -1,16 +1,78 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let input = (require("fs").readFileSync(filePath).toString().trim().split("\n"));
-const [house, load] = input[0].split(" ");
-const path = [];
-// const graph = Array.from({ length: load + 1 }).fill(0);
-let graph = Array.from(Array(Number(house) + 1), () => Array());
 
-for (let i = 1; i < input.length; i++){
-    path.push(input[i].split(" ").map(e => +e));
+function main() {
+    let answer = 0;
+    const [N, M] = input[0].split(' ').map(Number);
+    let road = input.slice(1).map(_ => _.trim().split(' ').map(Number));
+
+    let village = [];
+    for (let i = 0; i < road.length; i++) {
+        let [A, B, C] = road[i];  // A -> B : C.
+        village.push({
+            from: A,
+            to: B,
+            cost: C
+        })
+    }
+    village.sort((a, b) => {
+        a = a.cost;
+        b = b.cost;
+        if (a > b) return 1;
+        else if (a < b) return -1;
+    })
+    //console.log(village)
+
+    // unionFind
+    let parent = new Array(N + 1).fill(0);
+    for (let i = 0; i <= N; i++) {
+        parent[i] = i;
+    }
+
+    answer = sol(village, parent);
+
+    return console.log(answer)
 }
 
-path.forEach(([start, end, cost]) => {
-    graph[start].push([end, cost]);
-})
+function sol(village, parent) {
+    let sum = 0;
+    let MST = [];
+    
+    for (let i = 0; i < village.length; i++) {
+        let [a, b, c] = [village[i].to, village[i].from, village[i].cost];
+        // cycle check.
+        if (!findParent(a, b, parent)) {
+            unionParent(a, b, parent);
+            MST.push({
+                from: a,
+                to: b,
+                cost: c
+            })
+        }
+    }
+    // 비용이 젤 높은 간선 자르기.
+    MST.pop()
+    for (let i = 0; i < MST.length; i++) {
+        sum += MST[i].cost;
+    }
 
-// 포기
+    return sum;
+}
+function getParent(x, parent) {
+    if (parent[x] === x) return x;
+    else return parent[x] = getParent(parent[x], parent); // return 이 있어야 축약됨.
+}
+function findParent(a, b, parent) {
+    a = getParent(a, parent);
+    b = getParent(b, parent);
+    if (a === b) return true;
+    else return false;
+}
+function unionParent(a, b, parent) {
+    a = parent[a];
+    b = parent[b];
+    if (a < b) parent[b] = a;
+    else parent[a] = b;
+}
+
+main();
