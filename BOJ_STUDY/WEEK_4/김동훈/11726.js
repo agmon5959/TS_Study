8614ef74643cece5a979ea25559f90a61c03ac34
김동훈A <agmon5959@douzone.com>
2022-08-26 17:49:10 +0900

    11726

--- BOJ_STUDY/WEEK_4/김동훈/11726.js
+++ BOJ_STUDY/WEEK_4/김동훈/11726.js
@@ -1,3 +1,12 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let input = (require("fs").readFileSync(filePath).toString().trim().split("\n"));
 
+var answer = 0;
+var arr = [0, 1, 2];
+if (input > 2) {
+    for (var i = 3; i <= input; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+}
+answer = arr[input] % 10007;
+console.log(answer);
