deb5cbb7aaad04e0ec905f8718a8862d7b79087c
김동훈A <agmon5959@douzone.com>
2022-08-26 17:51:52 +0900

    오류 해결

--- BOJ_STUDY/WEEK_4/김동훈/11726.js
+++ BOJ_STUDY/WEEK_4/김동훈/11726.js
@@ -1,12 +1,11 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let input = (require("fs").readFileSync(filePath).toString().trim().split("\n"));
 
-var answer = 0;
+input = Number(input);
 var arr = [0, 1, 2];
 if (input > 2) {
     for (var i = 3; i <= input; i++) {
-        arr[i] = arr[i - 1] + arr[i - 2];
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
     }
 }
-answer = arr[input] % 10007;
-console.log(answer);
+console.log(arr[input]);
