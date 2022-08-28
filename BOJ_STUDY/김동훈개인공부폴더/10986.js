806d77ecc9554af801292599d224e3983b02d8de
김동훈A <agmon5959@douzone.com>
2022-08-29 07:28:51 +0900

    누적합 최종 해결 코드

--- BOJ_STUDY/김동훈개인공부폴더/10986.js
+++ BOJ_STUDY/김동훈개인공부폴더/10986.js
@@ -4,27 +4,21 @@ const [N, M] = input[0].split(" ").map(e => +e);
 const matrix = input[1].split(" ").map(e => +e);
 const remain = Array.from({ length: M }).fill(0);
 
+let ans = 0;
 
-remain[0]++;  // 초기값 설정해주기, 이거 없으면 NAN
-remain[matrix[0] % M]++;
-
-
-// console.log('remain >> ',remain);
-// console.log('matrix >> ',matrix);
 for (let i = 0; i < N; i++) {
     if (i === 0) continue;
-    matrix[i] += matrix[i - 1];
-    // console.log(matrix[i] % M);
-    remain[matrix[i] % M]++;
+    matrix[i] += matrix[i - 1];   
 }
 
-let ans = 0;
-console.log(remain);
+
+for (let i = 0; i < N; i++) {
+    remain[matrix[i] % M]++;
+    if (matrix[i] % M === 0) ans++;
+}
+
 for (let cnt of remain) {
     ans += cnt * (cnt - 1) / 2;
 }
 
-
-// console.log(matrix);
-// console.log(remain);
 console.log(ans);
