3db22f9a8d59a78e004c7b7b6524699ed7512bfe
김동훈A <agmon5959@douzone.com>
2022-08-28 19:23:05 +0900

    누적합 ㅠ

--- BOJ_STUDY/김동훈개인공부폴더/10986.js
+++ BOJ_STUDY/김동훈개인공부폴더/10986.js
@@ -2,30 +2,29 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let input = (require("fs").readFileSync(filePath).toString().trim().split("\n"));
 const [N, M] = input[0].split(" ").map(e => +e);
 const matrix = input[1].split(" ").map(e => +e);
-let ans = 0;
+const remain = Array.from({ length: M }).fill(0);
 
-const prefixSum = Array.from({ length: N + 1 }).fill(0);
 
+remain[0]++;  // 초기값 설정해주기, 이거 없으면 NAN
+remain[matrix[0] % M]++;
+
+
+// console.log('remain >> ',remain);
+// console.log('matrix >> ',matrix);
 for (let i = 0; i < N; i++) {
-    prefixSum[i + 1] = prefixSum[i] + matrix[i];
+    if (i === 0) continue;
+    matrix[i] += matrix[i - 1];
+    // console.log(matrix[i] % M);
+    remain[matrix[i] % M]++;
+}
+
+let ans = 0;
+console.log(remain);
+for (let cnt of remain) {
+    ans += cnt * (cnt - 1) / 2;
 }
 
 
-for (let i = 0; i <M; i++) {
-    prefixSum[i] = matrix[i] % M;
-}
-
-let temp_0 = 0;
-let temp_1 = 0;
-prefixSum.forEach((iter) => {
-    if (iter === 1) {
-        temp_1++;
-    } else {
-        temp_0++;
-    }
-})
-
-ans += temp_0 * (temp_0 - 1) / 2;
-ans += temp_1 * (temp_1 - 1) / 2;
-
+// console.log(matrix);
+// console.log(remain);
 console.log(ans);
