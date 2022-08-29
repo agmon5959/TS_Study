8535afe06f4557206e3ae41354ada14bbe44b283
parkdohyun <freesin94@gmail.com>
2022-08-29 21:33:29 +0900

    [BOJ] 11726번 문제 풀이

--- BOJ_STUDY/WEEK_4/박도현/BOJ_9095.py
+++ BOJ_STUDY/WEEK_4/박도현/BOJ_9095.py
@@ -1,9 +1,10 @@
 # test case 갯수
 T = int(input())
-d = [0] * 11
+# DP 테이블 초기화
+d = [0] * 10
 d[0], d[1], d[2] = 1, 1, 2
 
-#필요한 답 미리 저장
+# BOTTOM UP DP
 for i in range(3, 11):
     d[i] = d[i-1] + d[i-2] + d[i-3]
 
