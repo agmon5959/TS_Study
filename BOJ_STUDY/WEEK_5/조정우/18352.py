0667c87d8e1e7c6e25657ace68e77796f138ca8e
wjddn1029 <wjddn1029@kakao.com>
2022-09-06 23:51:44 +0900

    [week_5] 18352 특정거리 도시찾기 push bfs

---
+++ BOJ_STUDY/WEEK_5/조정우/18352.py
@@ -0,0 +1,38 @@
+from collections import deque
+import sys
+
+
+def bfs(x):
+    answer = []
+    q = deque([x])
+    visited[x] = True
+    distance[x] = 0
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            if not visited[i]:
+                visited[i] = True
+                q.append(i)
+                distance[i] = distance[now] + 1
+                if distance[i] == k:
+                    answer.append(i)
+    if len(answer) == 0:
+        print(-1)
+    else:
+        answer.sort()
+        for i in answer:
+            print(i, end='\n')
+
+
+f = sys.stdin.readline
+
+n, m, k, x = map(int, f().split())
+graph = [[] for _ in range(n+1)]
+distance = [0] * (n+1)
+visited = [False] * (n+1)
+
+for _ in range(m):
+    a, b = map(int, f().split())
+    graph[a].append(b)
+
+bfs(x)
