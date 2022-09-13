899cacddd1504a1f2cc2d652961d7f589d2b0630
김동훈A <agmon5959@douzone.com>
2022-09-14 07:32:30 +0900

    2667 commit

--- BOJ_STUDY/WEEK_6/김동훈/2667.js
+++ BOJ_STUDY/WEEK_6/김동훈/2667.js
@@ -1,2 +1,7 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let input = (require("fs").readFileSync(filePath).toString().trim().split("\n"));
+const size = +input.shift();
+const originMap = input;
+const targetMap = Array.from({ length: size } , Array.from(size));
+
+console.log(targetMap);
