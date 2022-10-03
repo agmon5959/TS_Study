99e44577d8648f6ec7c5eed6d1b3c3034b2978ee
김동훈A <agmon5959@douzone.com>
2022-10-04 04:52:00 +0900

    포기

--- BOJ_STUDY/WEEK_9/김동훈/6549.js
+++ BOJ_STUDY/WEEK_9/김동훈/6549.js
@@ -1,2 +1,28 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = (require("fs").readFileSync(filePath).toString().trim().split("\n"));
+const inputArr = input.map(iter => iter.split(' ').map(e => +e))
+let N = 0;
+const maxArr = []; // 최소값이 담긴다.
+
+inputArr.forEach((iter) => {
+    N = iter.shift();
+    let target = iter[0];
+
+    let p1 = 0;
+    let p2 = 0;
+    let width = 1;
+    let height = 0;
+
+    let maxValue = Math.max(N, ...iter);
+    for (let i = 1; i < N; i++){    
+        
+        if (target <= iter[i]) {
+            
+        } else {
+            
+        }
+    }
+
+    
+})
+console.log(maxArr);
