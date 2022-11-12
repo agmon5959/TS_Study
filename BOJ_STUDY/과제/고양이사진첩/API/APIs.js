58b9842b274d18f1501e6ac787caa77a3b2dbd84
김동훈A <agmon5959@douzone.com>
2022-11-12 21:26:58 +0900

    과제

--- BOJ_STUDY/과제/고양이사진첩/API/APIs.js
+++ BOJ_STUDY/과제/고양이사진첩/API/APIs.js
@@ -1,23 +1,28 @@
-// https://l9817xtkq3.execute-api.ap-northeast-2.amazonaws.com/dev/
-// method: GET
-// 사진첩의 root 경로에 있는 파일들과 디렉토리들을 불러옵니다.
-
-// 특정 디렉토리에 속하는 파일 / 디렉토리 불러오기
-// https://l9817xtkq3.execute-api.ap-northeast-2.amazonaws.com/dev/:nodeId
-// method: GET
-
-const API_END_POINT = "https://l9817xtkq3.execute-api.ap-northeast-2.amazonaws.com/dev/";
-
-const APIs = async (type) => {
-    if (type) {
-        const url = API_END_POINT;
-        const getRootDirAPI = await fetch(url, { method: "GET" })
-        debugger
-        if (getRootDirAPI.ok) {
-            debugger
-        }
-
+const API_END_POINT = "https://l9817xtkq3.execute-api.ap-northeast-2.amazonaws.com/dev/"
+const cache = {}
+const APIs = async (type, param) => {
+    let callAPI = "";
+    let url = "";
+    let json = null;
+    switch (type) {
+        case "root":
+            callAPI = await fetch(API_END_POINT, { method: "GET" });
+            break;
+        case "catDir":
+            url = `${API_END_POINT}+${param}`
+            callAPI = fetch(url, { method: "GET" });
+            break;
+        case "image":
+            url = `${API_END_POINT}+${param}`
+            callAPI = fetch(url, { method: "GET" });
+            break;
     }
+
+    if (callAPI.ok) {
+        json = callAPI.json();
+    }
+
+    return json;
 }
 
-export default APIs
+export default APIs;
--- BOJ_STUDY/과제/고양이사진첩/Components/Breadcrumb.js
+++ BOJ_STUDY/과제/고양이사진첩/Components/Breadcrumb.js
@@ -1,19 +1,19 @@
-function Breadcrumb({ target, initState }) {
-    this.state = initState;
+export default function Breadcrumb({ target, pState }) {
+    this.state = pState;
     this.element = document.createElement('nav');
     this.element.className = "Breadcrumb";
-    target.appendChild = this.element;
 
-    this.setState = (nextState) => {
-        this.state = nextState;
-        this.render();
+    target.appendChild(this.element);
+
+
+    this.setState = (pState) => {
+        this.state = pState;
     }
 
     this.render = () => {
-
+        this.element.innerHTML = `<div>root</div>`;
     }
 
-
+    this.render();
+    console.log('asd')
 }
-
-export default Breadcrumb;
